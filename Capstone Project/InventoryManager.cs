1f836816632175601c22a8449db8072c6375bc84
LouisGichiah <louiegpaul@gmail.com>
2025-05-10 16:38:13 +0300

    getting started

---
+++ Capstone Project/InventoryManager.cs
@@ -0,0 +1,9 @@
+using System;
+using System.Collections.Generic;
+class InventoryManager
+{
+    static void Main(string[]args)
+    {
+        static List<string> products = new List<string>();
+    }
+}
