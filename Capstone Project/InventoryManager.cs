ccd4db8b799f489956799226e38a364cea480ea4
LouisGichiah <louiegpaul@gmail.com>
2025-05-19 05:51:13 +0300

    updated addproduct features - while loop to repeat prompt until item is entered

--- Capstone Project/InventoryManager.cs
+++ Capstone Project/InventoryManager.cs
@@ -5,7 +5,7 @@ class InventoryManager
     static List<string> products = new List<string>();
     static List<string> productPrices = new List<string>();
     static List<string> productQuantities = new List<string>();
-    
+   
     static void Main(string[]args)
     {
     bool exit = false;
@@ -19,7 +19,7 @@ class InventoryManager
             Console.WriteLine("5. Exit");
             Console.WriteLine("\n Please select an option 1-5");
             string selectOption=Console.ReadLine();
-            
+           
             switch (selectOption)
             {
                 case "1":
@@ -29,54 +29,60 @@ class InventoryManager
                 case "2":
                 viewProducts();
                 break;
-                
+               
                 case "3":
                 removeProduct();
                 break;
-                
+               
                 case "4":
                 updateStock();
                 break;
-                
+               
                 case "5":
                 exit = true;
                 break;
-                
+               
                 default:
                 Console.WriteLine("Please enter a valid selection");
                 break;
-            } 
+            }
             static void addProduct()
             {
                 while(true)
                 {
                 Console.WriteLine("Enter the product: ");
                 string product = Console.ReadLine();
-                  if(product == "")
+                  while(product == "")
                     {
                     Console.WriteLine("Cannot leave the product description empty.");
+                    Console.WriteLine("Enter the product: ");
+                    product = Console.ReadLine();
                     continue;
                     }
-                
+               
                 Console.WriteLine("Enter the product Price: ");
                 string price = Console.ReadLine();
-                   if(price == "")
+                   while(price == "")
                     {
-                        Console.WriteLine("Cannot leave price and blank, Kindly enter the product price ");
+                        Console.WriteLine("Cannot leave price blank, Kindly enter the product price ");
+                        Console.WriteLine("Enter the product Price: ");
+                        price = Console.ReadLine();
                         continue;
                     }
                
                 Console.WriteLine("Enter the quantity to be added : ");
                 string quantity = Console.ReadLine();
-                    if(quantity  == "")
+                    while(quantity  == "")
                     {
                         Console.WriteLine("Cannot leave the quantity blank, Kindly enter the product quantity");
+                        Console.WriteLine("Enter the quantity to be added : ");
+                        quantity = Console.ReadLine();
                         continue;
                     }
                 products.Add(product);
                 productPrices.Add(price);    
                 productQuantities.Add(quantity);
-                
+               
                 Console.WriteLine("Product added succesfully!");
                 break;
                 }    
@@ -154,7 +160,7 @@ class InventoryManager
                          }
 
               int index = choice - 1;
-            
+           
                  Console.WriteLine("Do you want to 'sell' or 'restock' the product?");
                     string action = Console.ReadLine().Trim().ToLower();
 
@@ -191,7 +197,7 @@ class InventoryManager
                  productQuantities[index] = currentQuantity.ToString();
                          Console.WriteLine($"Stock updated. New quantity for {products[index]}: {currentQuantity}");
             }
-    
+   
         }
     }
 }
