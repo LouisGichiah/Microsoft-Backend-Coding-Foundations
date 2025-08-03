24fc440e965281e4b8c15311d7540442aaab26d5
LouisGichiah <louiegpaul@gmail.com>
2025-08-03 09:51:28 +0300

    comments

--- Asynchronous Programming/retrieve file data.cs
+++ Asynchronous Programming/retrieve file data.cs
@@ -4,9 +4,9 @@ public async Task ReadFileDataAsync(string filePath)
     {
         using (var reader = new StreamReader(filePath))
         {
-            string content = await reader.ReadToEndAsync();
+            string content = await reader.ReadToEndAsync(); //ensures the file is read asynchronously until the end of the file
             Console.WriteLine("File content read successfully:");
-            Console.WriteLine(content);
+            Console.WriteLine(content); // Display the content of the file after read to end of file is completed
         }
     }
     catch (Exception ex)
